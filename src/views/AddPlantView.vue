68ddca9931e9249c1c23436a2efc2577fb2e4cb1
Poverd <pierreallaire@outlook.com>
2023-02-01 15:31:14 +0100

    Ajouts d'un dropdown et d'un bouton

--- src/views/AddPlantView.vue
+++ src/views/AddPlantView.vue
@@ -1,19 +1,48 @@
 <template>
-    <div class="home">
-      <h1> Accueil </h1>
-      <p>Bienvenue sur votre application pour garder et faire garder vos plantes</p>
-    </div>
-  </template>
-  
-  <script>
-  import Navigation from '@/components/Navigation.vue';
-  import Footer from '@/components/Footer.vue'
-  
-  export default {
-    components: {
-      Navigation, 
-      Footer
-    }
+  <div class="home">
+    <h1> Faire garder sa plante</h1>
+    <p>
+
+    </p>
+    <label for="plants"></label>
+    <select name="plants" id="plants">
+      <optgroup label="Plantes">
+        <option value="especes">Espèces</option>
+        <option value="mimosa">Mimosa</option>
+        <option value="hortancia">Hortancia</option>
+        <option value="dahlia">Dahlia</option>
+      </optgroup>
+    </select>
+    <p>
+
+
+    </p>
+  </div>
+  <div id="add_plants">
+    <button v-on:click="button_add += 1">Ajouter une plante</button>
+  </div>
+</template>
+
+
+<script>
+import Navigation from '@/components/Navigation.vue';
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  methods: {
+    button_add() {
+      var addplants = new Vue({
+        el: '#add_plants',
+        data: {
+          counter: 0
+        }
+      })
+      }
+    },
+  components: {
+    Navigation,
+    Footer
   }
-  </script>
-  
+}
+</script>
