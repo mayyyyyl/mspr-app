e96152ea4f762b5f6f5e79a917d6b492a0901e1f
JuuLed <julien.ledoux@epsi.fr>
2023-02-01 15:05:42 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav> -->
-  <header>  <Navigation/> 
+  <header>
+    <Navigation/> 
   </header>
+
   <router-view/>
+
   <footer>
     <Footer/>
   </footer>
@@ -25,11 +28,19 @@ export default {
 </script>
 
 <style>
+
+
+
+
+
+
+
+/* ____________________________________________ */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: var(--blue);
   color: #2c3e50;
 }
 
@@ -37,12 +48,16 @@ nav {
   padding: 30px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+/* .logo-arosaje {
+  text-decoration: none;
+  font-family: cursive;
+  color: green;
+  text-shadow: 1px 1px 2px rgb(63, 217, 63), 0 0 1em rgb(63, 217, 63), 0 0 0.2em rgb(63, 217, 63);
+} */
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #3d7606;
 }
+
+
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,26 @@
 <template>
-    <nav class="navbar sticky-top bg-light">
+    <nav class="navbar sticky-top">
       <div class="container-fluid">
-      <div class='row'>
-        <div class='col-3'>
-        <a aria-current="page" href="/"><img class='icon' src="@/assets/logo.png"></a>
-        </div>
-        <div class='col-6'>
-          <p class='text'>A'rosa-je</p>
-        </div>
-        <div class='col-3'>
-          <button class="navbar-toggler dropDownButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+        <div class='row'>
+
+          <div class='col-3'>
+            <button class="navbar-toggler dropDownButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+
+          <div class='col-3 logo-div'>
+            <a aria-current="page" href="/" class='logo-arosaje'>
+              <img class='icon' src="@/assets/logo.png">
+              <span class='text'>A'rosa-je</span>
+            </a>
+          </div>
+         
+          
+
         </div>
       </div>
-      
-       
-      </div>
+
        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mb-2 mb-lg-0">
             <li class="nav-item">
@@ -26,26 +30,26 @@
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Propriétaire
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/ajouter_plante' class="nav-link">Ajouter une plante</router-link>
-                <router-link to='/suivi' class="nav-link">Suivi des plantes</router-link>
+              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
+                <router-link to='/ajouter_plante' class="nav-link link-ss-menu">Ajouter une plante</router-link>
+                <router-link to='/suivi' class="nav-link link-ss-menu">Suivi des plantes</router-link>
               </div>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Gardien
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/garder_des_plantes' class="nav-link">Garder des plantes</router-link>
-                <router-link to='/ajouter_suivi' class="nav-link">Ajouter le suivi</router-link>
+              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
+                <router-link to='/garder_des_plantes' class="nav-link link-ss-menu">Garder des plantes</router-link>
+                <router-link to='/ajouter_suivi' class="nav-link link-ss-menu">Ajouter le suivi</router-link>
               </div>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Botaniste
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/ajouter_conseil' class="nav-link">Ajouter un conseil</router-link>
+              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
+                <router-link to='/ajouter_conseil' class="nav-link link-ss-menu">Ajouter un conseil</router-link>
               </div>
             </li>
           </ul>
@@ -74,11 +78,75 @@
 </script>
 
 <style scoped>
+
+:root {
+  --light-color: #a7e09c;
+  --light2-color: #ceecc9;
+
+  --primary-color: #5fa819;
+  --second-color: #3d7606;
+}
+
+
 .navbar {
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     -moz-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     -webkit-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background: #ceecc9;
+    text-align: center;
+    font-weight: bold;
 }
+.navbar-sous-menu {
+  background: #dff5db;
+  text-align: center;
+  font-weight: none;
+  font-size: 0.7em;
+}
+.link-ss-menu {
+  color: gray !important;
+}
+
+.row {
+    width: 100%;
+}
+/* .logo-arosaje {
+  height: 80%;
+  display: flex;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+}
+.logo-arosaje img {
+  width: 24m;
+}
+.logo-arosaje span {
+  text-decoration: none;
+  font-size: 1.5em;
+} */
+.logo-div {
+  /* position: relative; */
+  text-align: center;
+  height: 4em;
+}
+.logo-arosaje {
+  position: relative;
+  text-decoration: none;
+  margin: auto;
+}
+.logo-arosaje img {
+  position: absolute;
+  left: 8em;
+  z-index: 1;
+  left: 15px;
+}
+.logo-arosaje span {
+  position: absolute;
+  z-index: 5;
+  font-size: 2em;
+  width: 10em;
+}
+
+
 .menuButton{
     height: 4em;
 }
