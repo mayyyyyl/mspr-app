93c72052918b9289fd247a9dca81e307f6bd983a
JuuLed <julien.ledoux@epsi.fr>
2023-02-01 15:10:04 +0100

    burder

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
       <div class="container-fluid">
         <div class='row'>
 
-          <div class='col-3'>
+          <div class='col-1'>
             <button class="navbar-toggler dropDownButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
