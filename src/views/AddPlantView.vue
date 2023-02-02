edc0903fd9ef4387d2d6186004eeefc21daf31f4
Poverd <pierreallaire@outlook.com>
2023-02-02 10:30:59 +0100

    mise à jour du dropdown

--- src/views/AddPlantView.vue
+++ src/views/AddPlantView.vue
@@ -1,4 +1,5 @@
 <template>
+  <form id="plant_id" @submit="checkForm" method="post">
   <div class="add_plant">
     <div class="container-fluid justify-content-md-center">
       <div class="row justify-content-md-center">
@@ -7,17 +8,24 @@
           <p>
 
           </p>
+          <div class="row">
+            <div class="col-12 pt-4">
+              <span id="title_bloc">Veuillez choisir l'espèce de votre plante</span>
+            </div>
+          </div>
           <label for="plants"></label>
-          <select class="dropdown" name="plants" id="plants">
-              <option value="especes">Espèces</option>
-              <option value="mimosa">Mimosa</option>
-              <option value="hortancia">Hortancia</option>
-              <option value="dahlia">Dahlia</option>
+          <select class="form-select" aria-label="Default select example" id="plant_id" required>
+            <option selected>-- Espèces --</option>
+            <option value="1">Mimosa</option>
+            <option value="2">Hortancia</option>
+            <option value="3">Dahlia</option>
           </select>
-          <p>
-
-
-          </p>
+          <div>
+            <p></p>
+            <div>
+              <p></p>
+            </div>
+          </div>
           <router-link to="/ajouter_plante" custom v-slot="{ navigate }">
             <button class="btn btn_green" @click="navigate" role="link">
               <img id="icon_btn" src="@/assets/plant_icon_bw.png">
@@ -27,6 +35,7 @@
       </div>
     </div>
   </div>
+  </form>
 </template>
 
 <script>
