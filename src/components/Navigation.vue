bfec4e8be66f0e8e63d9e1bef3f0db06880a02f8
Maylis Le Borgne <maylislb@hotmail.fr>
2023-02-04 17:36:17 +0100

    FIX responsive navbar, ajout style et font

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,26 +1,22 @@
 <template>
-    <nav class="navbar sticky-top">
+    <nav class="navbar sticky-top bg-light">
       <div class="container-fluid">
-        <div class='row'>
-
-          <div class='col-1 burger'>
-            <button class="navbar-toggler dropDownButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </div>
-
-          <div class='col-9 logo-div'>
-            <a aria-current="page" href="/" class='logo-arosaje'>
-              <img class='icon' src="@/assets/logo.png">
-              <span class='text'>A'rosa-je</span>
-            </a>
-          </div>
-         
-          
-
+      <div class='row'>
+        <div class='col-3'>
+          <button class="navbar-toggler dropDownButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class='col-3'>
+        <a aria-current="page" href="/"><img class='icon' src="@/assets/logo.png"></a>
+        </div>
+        <div class='col-6'>
+          <p class='text'>A'rosa-je</p>
         </div>
       </div>
-
+      
+       
+      </div>
        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mb-2 mb-lg-0">
             <li class="nav-item">
@@ -30,26 +26,26 @@
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Propriétaire
               </a>
-              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/ajouter_plante' class="nav-link link-ss-menu">Ajouter une plante</router-link>
-                <router-link to='/suivi' class="nav-link link-ss-menu">Suivi des plantes</router-link>
+              <div class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                <router-link to='/ajouter_plante' class="nav-link">Ajouter une plante</router-link>
+                <router-link to='/suivi' class="nav-link">Suivi des plantes</router-link>
               </div>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Gardien
               </a>
-              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/garder_des_plantes' class="nav-link link-ss-menu">Garder des plantes</router-link>
-                <router-link to='/ajouter_suivi' class="nav-link link-ss-menu">Ajouter le suivi</router-link>
+              <div class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                <router-link to='/garder_des_plantes' class="nav-link">Garder des plantes</router-link>
+                <router-link to='/ajouter_suivi' class="nav-link">Ajouter le suivi</router-link>
               </div>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Botaniste
               </a>
-              <div class="dropdown-menu navbar-sous-menu" aria-labelledby="navbarDropdown">
-                <router-link to='/ajouter_conseil' class="nav-link link-ss-menu">Ajouter un conseil</router-link>
+              <div class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                <router-link to='/ajouter_conseil' class="nav-link">Ajouter un conseil</router-link>
               </div>
             </li>
           </ul>
@@ -78,86 +74,14 @@
 </script>
 
 <style scoped>
-
-:root {
-  --light-color: #a7e09c;
-  --light2-color: #ceecc9;
-
-  --primary-color: #5fa819;
-  --second-color: #3d7606;
-}
-
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Marck+Script&display=swap');
 
 .navbar {
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     -moz-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     -webkit-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    background: #ceecc9;
-    text-align: center;
-    font-weight: bold;
 }
-.navbar-sous-menu {
-  background: #dff5db;
-  text-align: center;
-  font-weight: none;
-  font-size: 0.7em;
-}
-.link-ss-menu {
-  color: gray !important;
-  width: 30%;
-  margin: auto;
-}
-.link-ss-menu:hover {
-  color: green !important;
-  text-shadow: 1px 1px 2px lightgreen, 0 0 1em lightgreen, 0 0 0.2em lightgreen;
-}
-
-.row {
-    width: 100%;
-}
-.dropDownButton {
-  margin-left: 0.5em !important;
-  background: #a7e09c;
-  border: 1px solid silver !important;
-}
-/* .logo-arosaje {
-  height: 80%;
-  display: flex;
-  margin: auto;
-  justify-content: center;
-  align-items: center;
-}
-.logo-arosaje img {
-  width: 24m;
-}
-.logo-arosaje span {
-  text-decoration: none;
-  font-size: 1.5em;
-} */
-.logo-div {
-  /* position: relative; */
-  text-align: center;
-  height: 4em;
-}
-.logo-arosaje {
-  position: relative;
-  text-decoration: none;
-  margin: auto;
-}
-.logo-arosaje img {
-  position: absolute;
-  left: 8em;
-  z-index: 1;
-  left: 15px;
-}
-.logo-arosaje span {
-  position: absolute;
-  z-index: 5;
-  font-size: 2em;
-  width: 10em;
-}
-
-
 .menuButton{
     height: 4em;
 }
@@ -165,12 +89,9 @@
     position:sticky;
 }
 .icon {
-    max-height:50px;
-    margin-top: 10%;
-}
-.icons {
-    height: 30px;
-    margin-right: 5%;
+    max-height:40px;
+    margin-top: 45%;
+    padding-left: .5em;
 }
 .dropDownButton{
     margin-top:10%;
@@ -182,6 +103,28 @@
 .text{
     margin-top : 20px;
     margin-left: -10px;
-    font-family: 'kanit';
+    font-family:'Marck Script';
+    font-size: 27px;
+    padding-left: .3em;
 }
+nav a {
+  font-weight:500;
+  text-align: center;
+  color: #2c3e50;
+}
+.dropdown-menu {
+  border: none;
+}
+.dropdown-menu > .nav-link{
+  font-size: 13px;
+  color: #0e5e0b;
+}
+.dropdown-menu > .nav-link:hover{
+  background-color: #0e5e0b;
+  color: white;
+}
+.nav-link:hover {
+  color: #0e5e0b;
+}
+
 </style>
